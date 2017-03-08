6c256805122537c64bc92bc341cfb929beccdfe8
Kamigami55 <samkami@icloud.com>
2017-03-09 04:20:45 +0800

    Use dotenv to storage env settings

--- run.js
+++ run.js
@@ -65,46 +65,19 @@ This bot demonstrates many of the core features of Botkit:
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-
-if (!process.env.page_token) {
-    console.log('Error: Specify page_token in environment');
-    process.exit(1);
-}
-
-if (!process.env.verify_token) {
-    console.log('Error: Specify verify_token in environment');
-    process.exit(1);
-}
-
-if (!process.env.app_secret) {
-    console.log('Error: Specify app_secret in environment');
-    process.exit(1);
-}
-
+var dotenv = require('dotenv')
 var Botkit = require('./lib/Botkit.js');
 var os = require('os');
-var commandLineArgs = require('command-line-args');
 var localtunnel = require('localtunnel');
 
-const ops = commandLineArgs([
-      {name: 'lt', alias: 'l', args: 1, description: 'Use localtunnel.me to make your bot available on the web.',
-      type: Boolean, defaultValue: false},
-      {name: 'ltsubdomain', alias: 's', args: 1,
-      description: 'Custom subdomain for the localtunnel.me URL. This option can only be used together with --lt.',
-      type: String, defaultValue: null},
-   ]);
-
-if(ops.lt === false && ops.ltsubdomain !== null) {
-    console.log("error: --ltsubdomain can only be used together with --lt.");
-    process.exit();
-}
+dotenv.load()
 
 var controller = Botkit.facebookbot({
     debug: true,
     log: true,
-    access_token: process.env.page_token,
-    verify_token: process.env.verify_token,
-    app_secret: process.env.app_secret,
+    access_token: process.env.PAGE_TOKEN,
+    verify_token: process.env.VERIFY_TOKEN,
+    app_secret: process.env.APP_SECRET,
     validate_requests: true, // Refuse any requests that don't come from FB on your receive webhook, must provide FB_APP_SECRET in environment variables
 });
 
@@ -114,8 +87,8 @@ var bot = controller.spawn({
 controller.setupWebserver(process.env.port || 3000, function(err, webserver) {
     controller.createWebhookEndpoints(webserver, bot, function() {
         console.log('ONLINE!');
-        if(ops.lt) {
-            var tunnel = localtunnel(process.env.port || 3000, {subdomain: ops.ltsubdomain}, function(err, tunnel) {
+        if(process.env.LT_SUBDOMAIN) {
+            var tunnel = localtunnel(process.env.port || 3000, {subdomain: process.env.LT_SUBDOMAIN}, function(err, tunnel){
                 if (err) {
                     console.log(err);
                     process.exit();
